f7e144526855ff10f2386519d96a50f2fede04f2
Ant290 <ant290@live.co.uk>
2019-01-13 22:55:43 +0000

    first commit to build repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ngMasters
-Angular dice interaction app
+Angular dice interaction app loosely based on dice masters
+
+plan is to have a player with some health who can take some damage and own some dice!
---
+++ ngMasters/src/app/dice/dice.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dice',
+  templateUrl: './dice.component.html',
+  styleUrls: ['./dice.component.css']
+})
+export class DiceComponent implements OnInit {
+
+  // expose props for binding
+  dice = 'rolly';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
